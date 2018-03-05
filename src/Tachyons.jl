a07d0030c52a20cfd52ea325921bc29779baa16c
Shashi Gowda <shashi@juliacomputing.com>
2018-03-05 15:28:06 +0530

    Tachyons!

--- src/Tachyons.jl
+++ src/Tachyons.jl
@@ -1,5 +1,19 @@
 module Tachyons
 
-# package code goes here
+using WebIO
+
+export tachyons_css, @class_str
+
+const tachyons_css = Scope(imports=["/pkg/Tachyons/tachyons.min.css"])
+
+macro class_str(s)
+    :(function (elem)
+          if haskey(props(elem), :className)
+              elem(className=props(elem)[:className] * " " * $(esc(s)))
+          else
+              elem(className=$(esc(s)))
+          end
+      end)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using Tachyons
 using Base.Test
+using WebIO
+using Blink
+using Observables
+using Base.Test
 
-# write your own tests here
-@test 1 == 2
+notinstalled = !AtomShell.isinstalled()
+
+notinstalled && AtomShell.install()
+
+@testset "Blink mocks" begin
+    w = Window(Dict(:show => false))
+
+    elem = dom"div#test1"("hello, world") |> class"bg-yellow green" |> class"pa5"
+    body!(w, dom"div"(tachyons_css, elem))
+    sleep(5) # wait for it to render.
+
+    substrings = ["""<div id="test1" class="bg-yellow green pa5">hello, world</div>"""]
+    content = Blink.@js(w, document.body.innerHTML)
+    @test all(x->contains(content, x), substrings)
+    br = Blink.@js(w, window.getComputedStyle(document.querySelector("#test1"), null).getPropertyValue("background-color"))
+    @test br == "rgb(255, 215, 0)"
+end
+
+notinstalled && AtomShell.uninstall()
